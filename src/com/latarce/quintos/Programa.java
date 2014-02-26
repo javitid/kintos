a79e05e3fb85bb6b3508495c07021be342221260
Javier Garcia <javitid@gmail.com>
2014-02-26 15:19:40 +0100

    Añadidos carteles

--- src/com/latarce/quintos/Programa.java
+++ src/com/latarce/quintos/Programa.java
@@ -4,17 +4,24 @@ import com.latarce.kintos14.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.webkit.WebView;
 
 public class Programa extends Activity{
-	private TextView view;
+	private WebView view;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.programa);
 		
-		view = (TextView) this.findViewById(R.id.cartelView);
-	    view.setBackgroundResource(R.drawable.cartel13);
+		view = (WebView) this.findViewById(R.id.cartelView);
+	    //view.setBackgroundResource(R.drawable.cartel14a);
+		String urlData = "<body><img src=\"cartel14a.jpg\"><br><br><img src=\"cartel14b.jpg\"><br><br>" +
+				"<img src=\"cartel14c.jpg\"><br><br><img src=\"cartel14d.jpg\"><BR><BR><BR>Carteles creados por " +
+				"<b>Rodrigo Canal Arribas - 630 11 68 41</b></body>";
+		view.loadDataWithBaseURL("file:///android_asset/", urlData, "text/html", "utf-8", null);
+		view.setInitialScale(120);
+		view.getSettings().setSupportZoom(true);
+		view.getSettings().setBuiltInZoomControls(true);
 	}
 }
